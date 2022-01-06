74c2357122c58b5721f2ca366230d4088d46c2b8
Andrew Kal <andrewkalgit@gmail.com>
2022-01-06 14:25:52 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Output from './Output';
+import Mood from './Mood';
+import Weather from './Weather';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<Output></Output>
+			<Mood></Mood>
+			<Weather></Weather>
+		</>
+	);
 }
 
 export default App;
---
+++ src/Mood.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+export default function Mood(props) {
+	const [mood, setMood] = useState(0);
+
+	return (
+		<>
+			<ul>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+			</ul>
+		</>
+	);
+}
